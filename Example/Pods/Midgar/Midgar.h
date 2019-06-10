4ba8c6bba27a4d8181c1a94ddf1c5a2691a51915
Bastien Beurier <bastienbeurier@gmail.com>
2019-06-09 17:50:03 -0700

    Carthage support

---
+++ Example/Pods/Midgar/Midgar.h
@@ -0,0 +1,18 @@
+//
+//  Midgar.h
+//  Midgar
+//
+//  Created by Bastien Beurier on 6/9/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Midgar.
+FOUNDATION_EXPORT double MidgarVersionNumber;
+
+//! Project version string for Midgar.
+FOUNDATION_EXPORT const unsigned char MidgarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Midgar/PublicHeader.h>
+
+
---
+++ Example/Pods/MidgarCarthage/MidgarCarthage.h
@@ -0,0 +1,18 @@
+//
+//  MidgarCarthage.h
+//  MidgarCarthage
+//
+//  Created by Bastien Beurier on 6/9/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MidgarCarthage.
+FOUNDATION_EXPORT double MidgarCarthageVersionNumber;
+
+//! Project version string for MidgarCarthage.
+FOUNDATION_EXPORT const unsigned char MidgarCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MidgarCarthage/PublicHeader.h>
+
+
